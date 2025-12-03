e4dfe6b5a4376ea743f920de204cf6db50c55822
James Villanueva <jvillan314@gmail.com>
2025-12-02 23:41:13 -0800

    doing it

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("Welcome to my portfolio!");
+
+//Example: Change background color on click
+	document.body.addEventListener("click", () => {
+	document.body.style.backgroundColor = "#e0f7fa";
+     });
+

43dd2f2265223b980276a958558fa70a1a76602a
James Villanueva <jvillan314@gmail.com>
2025-12-16 22:08:14 -0800

    idk

--- sketch.js
+++ sketch.js
@@ -5,6 +5,9 @@ let holdTimer = null;
 function setup() {
   let canvas = createCanvas(windowWidth, windowHeight);
   canvas.position(0, 0);
+  canvas.style("position", "fixed");
+  canvas.style("top", "0");
+  canvas.style("left", "0");
   canvas.style("z-index", "-1");
 
   for (let i = 0; i < 80; i++) {
